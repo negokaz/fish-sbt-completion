eeac75e1ef03f5bdbaa46a7a593de0528409a518
negokaz <negokaz@gmail.com>
2017-07-17 06:34:02 +0900

    Refactored sbt-new completion

--- completions/sbt.fish
+++ completions/sbt.fish
@@ -1,29 +1,24 @@
-function __fish_construct_completions_for_sbt_new_g8_templates
-    # erase initial sbt-new completion
-    complete --erase --command sbt --exclusive --condition='__fish_seen_subcommand_from new'
+
+# sbt new
+
+function __fish_cache_or_print_sbt_new_g8_templates --description 'Print giter8 templates for sbt-new'
 
     # Set up cache directory
     if test -z $XDG_CACHE_HOME
         set XDG_CACHE_HOME $HOME/.cache
     end
-    mkdir -m 700 -p $XDG_CACHE_HOME/sbt-completions
+    set --local sbt_completion_cache_dir $XDG_CACHE_HOME/fish/sbt-completions
+    mkdir -m 700 -p $sbt_completion_cache_dir
 
-    set --local sbt_g8_templates_cache_file $XDG_CACHE_HOME/sbt-completions/sbt_new_g8_templates
+    set --local sbt_g8_templates_cache_file $sbt_completion_cache_dir/sbt_new_g8_templates
     set --local templates_url https://github.com/foundweekends/giter8/wiki/giter8-templates
 
-    if not find $sbt_g8_templates_cache_file -mtime -1
-        curl -s $templates_url | grep "\.g8<" | sed -E -e 's/<[^>]+>//g' -e 's/\(//g' -e 's/\)//g' > $sbt_g8_templates_cache_file
+    if not find $sbt_g8_templates_cache_file -mtime -1 ^ /dev/null
+        curl -s $templates_url | grep "\.g8<" | sed -E -e 's/<[^>]+>//g' -e 's/\(//g' -e 's/\)//g' | awk '{ name = $1; $1 = ""; print name, "\t", $0 }' > $sbt_g8_templates_cache_file
     end
 
-    for template in (cat $sbt_g8_templates_cache_file)
-        # create sbt-new completion contains description
-        set --local template_name   (echo "$template" | awk '{print $1}')
-        set --local description     (echo "$template" | awk '{$1 = ""; print $0}')
-        complete --command sbt --exclusive --condition='__fish_seen_subcommand_from new' --arguments="$template_name" --description="$description"
-        # for initial completion
-        echo $template_name
-    end
+    cat $sbt_g8_templates_cache_file
 end
 
-# Initial sbt-new completion
-complete --command sbt --exclusive --condition='__fish_seen_subcommand_from new' --arguments='(__fish_construct_completions_for_sbt_new_g8_templates)'
+complete --command sbt --exclusive --condition='__fish_use_subcommand' --arguments='new' --description='Create new project from a template'
+complete --command sbt --exclusive --condition='__fish_seen_subcommand_from new' --arguments='(__fish_cache_or_print_sbt_new_g8_templates)'
